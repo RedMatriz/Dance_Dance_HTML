16d7d48e91a99e1030d6fc897f9e3afbca780ac6
TestProg <myyl815@gmail.com>
2020-02-05 18:38:56 -0600

    slight revamp

--- gamestuff/test prij script.js
+++ gamestuff/test prij script.js
@@ -1,89 +1,15 @@
-document.addEventListener("keypress", function (event) {
-    // if (event.key === 'a')
-    //     document.getElementById("changable").innerText = "something has changed " + event.keyCode;
-    if (event.key === 'a') {
-        document.getElementById("key0").style = "color:red;";
-        document.getElementById("keyh0").style = "background-color:grey;";
-        document.getElementById("key0").innerText = "A";
+function addListeners(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        document.addEventListener("keypress", function (event) {
+            if (event.key === arr[i].key)
+                document.getElementById(arr[i].keyId).style = "background-color:grey;color:red;";
+        });
+        document.addEventListener("keyup", function (event) {
+            if (event.key === arr[i].key)
+                document.getElementById(arr[i].keyId).style = "";
+        });
     }
-    if (event.key === 's') {
-        document.getElementById("key1").style = "color:red;";
-        document.getElementById("keyh1").style = "background-color:grey;";
-        document.getElementById("key1").innerText = "S";
-    }
-    if (event.key === 'd') {
-        document.getElementById("key2").style = "color:red;";
-        document.getElementById("keyh2").style = "background-color:grey;";
-        document.getElementById("key2").innerText = "D";
-    }
-    if (event.key === 'f') {
-        document.getElementById("key3").style = "color:red;";
-        document.getElementById("keyh3").style = "background-color:grey;";
-        document.getElementById("key3").innerText = "F";
-    }
-    if (event.key === 'j') {
-        document.getElementById("key4").style = "color:red;";
-        document.getElementById("keyh4").style = "background-color:grey;";
-        document.getElementById("key4").innerText = "J";
-    }
-    if (event.key === 'k') {
-        document.getElementById("key5").style = "color:red;";
-        document.getElementById("keyh5").style = "background-color:grey;";
-        document.getElementById("key5").innerText = "K";
-    }
-    if (event.key === 'l') {
-        document.getElementById("key6").style = "color:red;";
-        document.getElementById("keyh6").style = "background-color:grey;";
-        document.getElementById("key6").innerText = "L";
-    }
-    if (event.key === ';') {
-        document.getElementById("key7").style = "color:red;";
-        document.getElementById("keyh7").style = "background-color:grey;";
-        document.getElementById("key7").innerText = ";";
-    }
-});
-document.addEventListener("keyup", function (event) {
-    if (event.key === 'a') {
-        document.getElementById("key0").style = "";
-        document.getElementById("keyh0").style = "";
-        document.getElementById("key0").innerText = "A";
-    }
-    if (event.key === 's') {
-        document.getElementById("key1").style = "";
-        document.getElementById("keyh1").style = "";
-        document.getElementById("key1").innerText = "S";
-    }
-    if (event.key === 'd') {
-        document.getElementById("key2").style = "";
-        document.getElementById("keyh2").style = "";
-        document.getElementById("key2").innerText = "D";
-    }
-    if (event.key === 'f') {
-        document.getElementById("key3").style = "";
-        document.getElementById("keyh3").style = "";
-        document.getElementById("key3").innerText = "F";
-    }
-    if (event.key === 'j') {
-        document.getElementById("key4").style = "";
-        document.getElementById("keyh4").style = "";
-        document.getElementById("key4").innerText = "J";
-    }
-    if (event.key === 'k') {
-        document.getElementById("key5").style = "";
-        document.getElementById("keyh5").style = "";
-        document.getElementById("key5").innerText = "K";
-    }
-    if (event.key === 'l') {
-        document.getElementById("key6").style = "";
-        document.getElementById("keyh6").style = "";
-        document.getElementById("key6").innerText = "L";
-    }
-    if (event.key === ';') {
-        document.getElementById("key7").style = "";
-        document.getElementById("keyh7").style = "";
-        document.getElementById("key7").innerText = ";";
-    }
-});
+}
 
 function startGame() {
     const canvas = document.getElementById("game");
@@ -93,83 +19,29 @@ function startGame() {
     var timer = setInterval(uGame, 20);
 }
 
-var x = screen.width / 8, y = 0;
-var xchange = 0, ychange = 20;
 var timecount = 0;
-var blocks = [
-    {
-        x: screen.width / 8,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: true
-    },
-    {
-        x: screen.width / 8 + screen.width * .09375,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: false
-    },
-    {
-        x: screen.width / 8 + screen.width * .09375 * 2,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: false
-    },
-    {
-        x: screen.width / 8 + screen.width * .09375 * 3,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: false
-    },
-    {
-        x: screen.width / 8 + screen.width * .09375 * 4,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: false
-    },
-    {
-        x: screen.width / 8 + screen.width * .09375 * 5,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: false
-    },
-    {
-        x: screen.width / 8 + screen.width * .09375 * 6,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: false
-    },
-    {
-        x: screen.width / 8 + screen.width * .09375 * 7,
-        y: 0,
-        xchange: 0,
-        ychange: 20,
-        enabled: false
-    },
-];
+var blocks = [];
 
 function uGame() {
     const ctx = document.getElementById("game").getContext("2d");
     timecount += 1;
+    if ((Math.random() * 50).toFixed(0) * 1 === 27) {
+        blocks.push({
+            x: screen.width / 8 + screen.width * .09375 * (Math.random() * 7).toFixed(0),
+            y: 0,
+            xchange: 0,
+            ychange: 10,
+            enabled: true
+        });
+    }
     for (let i = 0; i < blocks.length; i++) {
-        if(timecount%(Math.random()*9+2).toFixed(0) === 0)
-            blocks[i].enabled = true;
-        if (blocks[i].enabled === true) {
-            ctx.clearRect(blocks[i].x, blocks[i].y, screen.width * .09375, 50);
-            blocks[i].y += ychange;
-            blocks[i].x += xchange;
-            ctx.fillRect(blocks[i].x, blocks[i].y, screen.width * .09375, 50);
-        }
-        if (blocks[i].y > window.innerHeight){
-            blocks[i].enabled = false;
-            blocks[i].y = 0;
+        ctx.clearRect(blocks[i].x, blocks[i].y, screen.width * .09375, 50);
+        blocks[i].y += blocks[i].ychange;
+        blocks[i].x += blocks[i].xchange;
+        ctx.fillRect(blocks[i].x, blocks[i].y, screen.width * .09375, 50);
+        if (blocks[i].y > window.innerHeight) {
+            blocks.splice(i, 1);
+            i--;
         }
     }
 
