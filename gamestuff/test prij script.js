fe8b43bad902a89df8d4385081140e501369e33f
Alan <alan.wan01@gmail.com>
2020-02-11 12:19:38 -0600

    ff

--- gamestuff/test prij script.js
+++ gamestuff/test prij script.js
@@ -88,6 +88,8 @@ function uGame() {
     const canvas = document.getElementById("game");
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "30px Ariel";
+    ctx.fillText("Score: " + score, 10, 50, screen.width / 8);
     timecount += 1;
     if ((Math.random() * 5).toFixed(0) * 1 === 3) {
         const loc = (Math.random() * 5).toFixed(0);
@@ -104,8 +106,6 @@ function uGame() {
     for (let i = 0; i < blocks.length; i++) {
         uColumn(blocks[i], ctx);
     }
-    ctx.font = "30px Ariel";
-    ctx.fillText("Score: " + score, 10, 50, screen.width / 8);
 }
 
 function uColumn(arr, ctx) {
